bfcd082088283d20fe761d9a01ec7d647f34df20
Samuel Dillow <tarwich@gmail.com>
2019-07-05 18:05:06 -0500

    Fixed to be even more versatile. Added tests

--- src/lib/docker.js
+++ src/lib/docker.js
@@ -2,80 +2,135 @@ const { log } = require('../log');
 const { spawnSync } = require('child_process');
 const { sync: globSync } = require('glob');
 const { resolve } = require('path');
+const { compareTwoStrings } = require('string-similarity');
 
 const { env } = process;
+
+/** @type {{
+ *   [key: string]: {
+ *     name: string;
+ *     ports: {host: string, container: string}[];
+ *     hostPorts: string[];
+ *     containerPorts: string[];
+ *   }
+ * }} */
+let dockerProcesses;
+
+/**
+ * @template T inferred
+ * @param {T | undefined} input
+ * @return {input is Exclude<T, undefined | null | '' | false | 0>}
+ */
+function isTruthy(input) {
+  return Boolean(input);
+}
 
 async function getDockerUrls() {
   if (!globSync(resolve('docker-compose.yml')).length) {
     env.DOCKER = 'ignore';
     log('docker', 'No docker-compose.yml. Not running docker');
+    return;
   }
 
-  if (!env.DOCKER) {
-    {
-      log('docker', 'Boot docker');
-      const shell = spawnSync('docker-compose', ['up', '-d']);
-      log('docker', String(shell.stderr));
-    }
+  if (env.DOCKER) return env.DOCKER;
 
-    {
-      log('docker', 'Get docker ports');
-      const shell = spawnSync('docker-compose', ['ps']);
-      log('docker', String(shell.stderr));
+  log('docker', 'Boot docker');
+  log('docker', String(spawnSync('docker-compose', ['up', '-d']).stderr));
 
-      if (shell.stdout) {
-        const services = String(shell.stdout)
-          .split('\n')
-          .map(line => {
-            const [, name = ''] = line.match(/_([^_]+?)_\d+/) || [];
-            /** @type {{[key: string]: string}} */
-            const ports = (line.match(/\d+->\d+/g) || [])
-              .map(assignment => assignment.match(/\d+/g) || [])
-              .reduce(
-                (ports, [container, host]) => ({
-                  ...ports,
-                  [host]: container,
-                }),
-                {}
-              );
-            /** @type {string?} */
-            const defaultPort = Object.values(ports)[0];
-            return { name, ports, defaultPort };
-          })
-          .filter(service => Boolean(service.defaultPort));
+  log('docker', 'Get docker ports');
+  const shell = spawnSync('docker-compose', ['ps']);
+  log('docker', String(shell.stderr));
 
-        // Add a fake service to the front of the array to handle a generic
-        // DATABASE_URL
-        if (services.length)
-          services.unshift({
-            name: 'database',
-            ports: {},
-            defaultPort: services[0].defaultPort,
-          });
+  const processes = await getDockerProcesses();
 
-        services.forEach(service => {
-          const { name, ports, defaultPort } = service;
-          const uname = name.toUpperCase();
-          const envKey = `${uname}_URL`;
-          env[`${uname}_PORT`] = defaultPort || '';
-          const envValue = env[envKey];
+  if (!processes) return;
 
-          if (envValue) {
-            const [, envPort = ''] = envValue.match(/:(\d+)/) || [];
-            const servicePort = ports[envPort] || service.defaultPort;
-            env[envKey] = envValue.replace(`:${envPort}`, `:${servicePort}`);
-            console.log('docker', `${envKey}: ${env[envKey]}`);
-          } else
-            console.log(
-              'docker',
-              `${service.name} port: ${service.defaultPort}`
-            );
-        });
-      }
-    }
+  replaceEnvVars(processes);
 
-    env.DOCKER = 'done';
-  }
+  env.DOCKER = 'done';
 }
 
-module.exports = { getDockerUrls };
+/**
+ * @param {typeof dockerProcesses} processes
+ */
+function replaceEnvVars(processes) {
+  Object.entries(process.env)
+    .filter(([envKey]) => /_URL$/.test(envKey))
+    .forEach(([envKey, envValue = '']) => {
+      const envTag = envKey.replace(/_URL$/, '').toLowerCase();
+      const [, envPort = ''] = envValue.match(/:(\d+)/) || [];
+
+      const candidates = Object.values(processes)
+        .map(process => {
+          const port = process.ports.find(port => port.container === envPort);
+          if (!port) return null;
+          const similarity = compareTwoStrings(envTag, process.name);
+
+          return { ...process, port, similarity };
+        })
+        .filter(isTruthy)
+        .sort(
+          (a, b) => b.similarity - a.similarity || a.name.length - b.name.length
+        );
+
+      const winner = candidates[0];
+
+      // console.log({ envKey, envValue, envTag });
+      // console.log('candidates', JSON.stringify(candidates, null, '  '));
+
+      if (winner) {
+        process.env[envKey] = envValue.replace(envPort, winner.port.host);
+      }
+    });
+}
+
+async function getDockerProcesses() {
+  if (!globSync(resolve('docker-compose.yml')).length) {
+    env.DOCKER = 'ignore';
+    log('docker', 'No docker-compose.yml. Not running docker');
+    return;
+  }
+
+  if (dockerProcesses) return dockerProcesses;
+
+  log('docker', 'Boot docker');
+  log('docker', String(spawnSync('docker-compose', ['up', '-d']).stderr));
+
+  log('docker', 'Get docker ports');
+  const shell = spawnSync('docker-compose', ['ps']);
+  log('docker', String(shell.stderr));
+
+  return parseDockerPs(String(shell.stdout));
+}
+
+/**
+ *
+ * @param {string} psString
+ * @return {typeof dockerProcesses}
+ */
+function parseDockerPs(psString = '') {
+  return (psString.match(/^\w+.*:\d+->\d+/gm) || []).reduce((results, line) => {
+    const [, name = ''] = line.match(/^.*?_(.+?)_/) || [];
+    const ports = (line.match(/:\d+->\d+/g) || []).map(match => {
+      const [host = '', container = ''] = match.match(/\d+/g) || [];
+      return { host, container };
+    });
+
+    return {
+      ...results,
+      [name]: {
+        name,
+        ports,
+        hostPorts: ports.map(port => port.host),
+        containerPorts: ports.map(port => port.container),
+      },
+    };
+  }, {});
+}
+
+module.exports = {
+  getDockerProcesses,
+  getDockerUrls,
+  parseDockerPs,
+  replaceEnvVars,
+};
